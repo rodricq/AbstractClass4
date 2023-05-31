e4188d5cabccf9aa65cd66af1967502417a68589
beverley-dick <beverleydick11@gmail.com>
2023-05-31 11:40:53 +0300

    Add project files.

---
+++ AbstractClass4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AbstractClass4
+{
+    abstract class Animal
+    {
+        public Animal()
+        {
+            Console.WriteLine("Animal Constructor");
+        }
+    }
+    class Dog : Animal
+    {
+        public Dog()
+        {
+            Console.WriteLine("Dog Constructor");
+        }
+    }
+    class Program
+    {
+        static void Main (string [] args)
+        {
+            //create an object of Dog class
+            Dog d1 = new Dog();
+
+            Console.ReadLine();
+        }
+    }
+}
